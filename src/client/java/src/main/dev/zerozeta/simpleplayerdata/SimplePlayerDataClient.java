700fdc808469b099d5ce56f7d988ee4ab5b80f14
0Zeta <0Zeta@protonmail.com>
2020-10-11 19:43:19 +0200

    Implement Cape URL retrieval for the Java client

--- src/client/java/src/main/dev/zerozeta/simpleplayerdata/SimplePlayerDataClient.java
+++ src/client/java/src/main/dev/zerozeta/simpleplayerdata/SimplePlayerDataClient.java
@@ -1,19 +1,29 @@
 package main.dev.zerozeta.simpleplayerdata;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.json.JSONArray;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SimplePlayerDataClient {
 
     private final String API_URL;
+    private final Gson GSON;
+    private final Type capeMapType = new TypeToken<HashMap<String, String>>() {
+    }.getType();
 
     /**
      * an interface to the simple player data API
@@ -22,7 +32,8 @@ public class SimplePlayerDataClient {
      * @param port the port number under which the service is running
      */
     public SimplePlayerDataClient(String host, String port) {
-        API_URL = "http://" + host + ":" + port + "/api/players";
+        API_URL = "http://" + host + ":" + port + "/api";
+        GSON = new Gson();
     }
 
     /**
@@ -35,9 +46,9 @@ public class SimplePlayerDataClient {
         URL url;
         try {
             if (playerName == null) {
-                url = new URL(API_URL);
+                url = new URL(API_URL + "/players");
             } else {
-                url = new URL(API_URL + "?player=" + playerName);
+                url = new URL(API_URL + "/players?player=" + playerName);
             }
         } catch (MalformedURLException ex) {
             ex.printStackTrace();
@@ -51,18 +62,18 @@ public class SimplePlayerDataClient {
             con.setRequestMethod("GET");
 
             if (con.getResponseCode() != 200) {
-                return new ArrayList<String>();
+                return new ArrayList<>();
             }
             BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
             response = reader.readLine();  // should contain only one line
 
         } catch (IOException ex) {
             ex.printStackTrace();
-            return new ArrayList<String>();
+            return new ArrayList<>();
         }
 
         JSONArray json = new JSONArray(response);
-        List<String> playerList = new ArrayList<String>();
+        List<String> playerList = new ArrayList<>();
         for (int i = 0; i < json.length(); i++) {
             playerList.add(json.getString(i));
         }
@@ -76,5 +87,51 @@ public class SimplePlayerDataClient {
      */
     public List<String> getPlayerList() {
         return getPlayerList(null);
+    }
+
+    /**
+     * fetches the cape URLs for the players with the given UUIDs
+     *
+     * @param uuids the UUIDs to check for capes
+     * @return a map of the cape URLs for the provided UUIDs
+     */
+    public Map<String, String> getCapeURLs(Iterable<String> uuids) {
+        Map<String, String> urls = new HashMap<>();
+        URL url;
+        try {
+            url = new URL(API_URL + "/capes");
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        HttpURLConnection con;
+        String response;
+        try {
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+
+            String uuidString = "{\"players\": " + GSON.toJson(uuids) + "}";
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = uuidString.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            if (con.getResponseCode() != 200) {
+                return urls;
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            response = reader.readLine();  // should contain only one line
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return urls;
+        }
+
+        urls = GSON.fromJson(response, capeMapType);
+        return urls;
     }
 }
